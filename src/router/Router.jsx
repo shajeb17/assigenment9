b8f88d7ff49e893e9f23aef0ab1003dd18ca94b4
unknown <shajeb17.11@gmail.com>
2025-11-20 10:06:08 +0600

    first commit

---
+++ src/router/Router.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router";
+import MainLayout from "../layout/MainLayout";
+
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: MainLayout,
+  },
+]);
