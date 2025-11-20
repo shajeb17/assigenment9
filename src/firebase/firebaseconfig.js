b140415b73015121f1f33c024201629b13c7bc52
unknown <shajeb17.11@gmail.com>
2025-11-20 10:41:34 +0600

    environment-vairble

--- src/firebase/firebaseconfig.js
+++ src/firebase/firebaseconfig.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDuKD8emgWxKp4uK7i1gWvVRkMQiKXnggE",
-  authDomain: "assigenment9.firebaseapp.com",
-  projectId: "assigenment9",
-  storageBucket: "assigenment9.firebasestorage.app",
-  messagingSenderId: "940044179958",
-  appId: "1:940044179958:web:b15844f11cd0d62eb6cfd7"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
